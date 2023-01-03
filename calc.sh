7568c8b631b87d851a8c420b10c034e12250a11e
shoval <shoval123055@gmail.com>
2023-01-03 15:43:18 +0200

    test 1

--- calc.sh
+++ calc.sh
@@ -5,4 +5,10 @@ Version=$(git describe --tags | cut -d '-' -f1 | awk -F. -v OFS=. '{$NF += 1 ; p
 if [ "${Version}" = "" ];then
 Version="$1.1"
 fi
+Version_Test=$(echo $Version | cut -d '.' -f1-2)
+if [ "${Version_Test}" != "${Version}" ];then
+Version="$1.1"
+fi
+
+
 echo $Version
